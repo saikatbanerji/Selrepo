b0fac0955981bdfc63fc32cdaff23a5a8692ae22
sairam <sairam@sairam-PC>
2018-06-01 08:18:07 +0530

    commit proj

--- webapp-newaddressbook/src/test/java/TestNg/addressbook.java
+++ webapp-newaddressbook/src/test/java/TestNg/addressbook.java
@@ -9,7 +9,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class addressbook {
+public class addressbook  {
 	WebDriver driver;
   @BeforeMethod
   public void launch() {
